2daaea71f50b01869696190c2bfa39f90b7125f9
vilxn <ibrahimamankelby@gmail.com>
2026-01-30 18:32:15 +0500

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text>Главная страница</Text>
+        </View>
+    );
+}
