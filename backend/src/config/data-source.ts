ffd6ab1aeb030f2bc182addddf76350c73c71cd8
vilxn <ibrahimamankelby@gmail.com>
2026-02-01 06:24:31 +0500

    backedn init

---
+++ backend/src/config/data-source.ts
@@ -0,0 +1,12 @@
+import { DataSource } from 'typeorm';
+import { User } from '../entities/User';
+
+export const AppDataSource = new DataSource({
+    type: 'sqlite',
+    database: 'database.sqlite',
+    synchronize: true,
+    logging: true,
+    entities: [User],
+    subscribers: [],
+    migrations: [],
+});
